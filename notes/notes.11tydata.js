66a38fb0a49de28f8f36a8085acc3c710de64297
Binyamin Aron Green <binyamingreen@protonmail.com>
2020-10-05 12:42:11 -0400

    Update notes metadata script
    See binyamin/binyam.in@e0e067d

--- notes/notes.11tydata.js
+++ notes/notes.11tydata.js
@@ -1,48 +1,56 @@
 const {titleCase} = require("title-case");
 const path = require("path");
 
+const wikilinkRegExp = /\[\[([\w\s/-]+)(.\w+)?\s?(\|([\w\s/]+))?\]\]/g
+
+function removeFrontmatter(content="") {
+    content = content.trimStart();
+    let yamlFm = content.substring(3, content.indexOf("---", 3)).trim();
+
+    if(!yamlFm || !content.startsWith("---")) return content; // Content has no frontmatter
+
+    return content.replace(`---\n${yamlFm}\n---`, "");
+}
+
 module.exports = {
     layout: "note.html",
     type: "note",
     eleventyComputed: {
-        title: data => titleCase(data.page.fileSlug),
+        title: data => titleCase(data.title || data.page.fileSlug),
         backlinks: (data) => {
             const notes = data.collections.notes;
             const currentFileSlug = data.page.fileSlug;
 
-            const stripYaml = (content) => {
-                if(content.startsWith("---")) {
-                    return content.substr(content.indexOf("---", 3) + 3);
-                } else {
-                    return content;
-                }
-            }
-
             // Search each note for backlinks
             return notes.filter(n => {
                 // Only fetch backlinks
-                const noteContent = stripYaml(n.template.inputContent);
+                const noteContent = removeFrontmatter(n.template.inputContent);
 
                 // This regex finds all wikilinks in the note
-                const linksInNote = (noteContent.match(
-                    /\[\[([\w\s/-]+)(.\w+)?(\|([\w\s/]+))?\]\]/g
-                ) || [])
+                const linksInNote = (noteContent.match(wikilinkRegExp) || [])
                 .map(m => (
                     // Extract link location
-                    m.slice(2,-2).split("|")[0]
+                    m.slice(2,-2)
+                        .split("|")[0]
                         .toLowerCase()
-                        .replace(/[^\w\s-]+/g,'')
+                        .replace(/[^\w\s/-]+/g,'')
                 ));
 
                 return linksInNote.includes(currentFileSlug);
             }).map(n => {
                 // Construct return object
-                const noteContent = stripYaml(n.template.inputContent);
+                const noteContent = removeFrontmatter(n.template.inputContent);
+                
+                // Truncate noteContent for preview
+                let preview = noteContent.slice(0, 200);
+
+                // truncate preview further, to last period
+                preview = preview.slice(0, preview.lastIndexOf(".") +1);
                 
                 return {
                     url: n.url,
-                    title: n.data.title || titleCase(path.basename(n.filePathStem)),
-                    preview: noteContent
+                    title: n.data.title,
+                    preview
                 }
             })
         }
